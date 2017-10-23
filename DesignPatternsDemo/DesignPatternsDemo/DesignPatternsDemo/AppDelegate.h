6dc2a9e961e4706c8a0ae65314b20ff3210ef252
zpz5789 <1059491105@qq.com>
2017-10-23 16:22:43 +0800

    creat DesignPatternsDemo group and some relative folders

---
+++ DesignPatternsDemo/DesignPatternsDemo/DesignPatternsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesignPatternsDemo
+//
+//  Created by zpz on 2017/10/23.
+//  Copyright © 2017年 zpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
