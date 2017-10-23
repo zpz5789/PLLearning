d29cf7e300dcc8acceaafe721617fed857039244
zpz5789 <1059491105@qq.com>
2017-10-23 16:31:34 +0800

    create designpatternsDemo

---
+++ AnimationDemo/AnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationDemo
+//
+//  Created by zpz on 2017/7/27.
+//  Copyright © 2017年 zpzDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationDemo/AnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationDemo
+//
+//  Created by zpz on 2017/7/27.
+//  Copyright © 2017年 zpzDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
