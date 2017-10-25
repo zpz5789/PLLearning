be4e57c80d0dd64c829e4fbee0cf9908a72801f8
zpz5789 <1059491105@qq.com>
2017-10-25 11:29:56 +0800

    design pattern viewcontroller build

--- DesignPatternsDemo/DesignPatternsDemo/DesignPatternsDemo/ViewController.m
+++ DesignPatternsDemo/DesignPatternsDemo/DesignPatternsDemo/ViewController.m
@@ -8,17 +8,73 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource>
+@property(nonatomic, strong) UITableView *tableView;
+@property(nonatomic, strong) NSArray *patternNames;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    //AbstractFactory
     [super viewDidLoad];
+    [self loadData];
+    [self buildUI];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+#pragma mark - TableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _patternNames.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifer = @"cellID";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifer];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifer];
+    }
+    cell.textLabel.text = [_patternNames objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - privte
+- (void)loadData
+{
+    _patternNames =  @[
+                       @"SimpleFactory",
+                       @"FactoryMethod",
+                       @"AbstractFactory",
+                       @"Singleton",
+                       @"Prototype",
+                       @"Builder",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       @"",
+                       ];
+}
+
+
+- (void)buildUI
+{
+    self.title = @"DesignPatterns";
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
